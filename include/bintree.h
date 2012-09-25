08fe682506c30170b47a65d50f0742c523c2e67f
Michael Tuttle <tuttlem@gmail.com>
2012-09-26 08:28:52 +1000

    Header for binary tree

---
+++ include/bintree.h
@@ -0,0 +1,50 @@
+
+#ifndef __libced_bintree_h_
+
+#define __libced_bintree_h_
+
+#include <stdlib.h>
+
+/*
+ * Binary tree implementation
+ * http://en.wikipedia.org/wiki/Binary_tree
+ */
+
+/**
+ * b-tree comparator function signature
+ */
+typedef int(*bintree_cmp)(const void *, const void *);
+
+/**
+ * @struct bintree_node_t
+ * #brief Defines the structure of a b-tree node
+ */
+typedef struct bintree_node_t {
+   struct bintree_node_t *left;  /* left leaf node */
+   struct bintree_node_t *right; /* right leaf node */
+
+   void *data;                 /* data at this node */
+} bintree_node;
+
+/**
+ * @struct bintree_t
+ * @brief Defines a b-tree structure
+ */
+typedef struct bintree_t {
+   bintree_cmp             comp; /* the comparator used */
+   struct bintree_node_t  *root; /* root node of the tree */
+} bintree;
+
+/**
+ * Creates a binary tree
+ * @param c The node comparator
+ */
+bintree* bintree_create(bintree_cmp c);
+
+/**
+ * Destroyes a binary tree
+ * @param t The tree to destroy
+ */
+int bintree_destroy(bintree **t);
+
+#endif /* __libced_bintree_h_ */
